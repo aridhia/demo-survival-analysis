8c40fbaa2bf0af2f77c5e5c7866fe48b5e522a46
Anna Domenech <annadomenech@Aridhia.net>
2020-11-04 16:09:08 +0000

    added DESCRIPTION file

--- code/help_tab.R
+++ code/help_tab.R
@@ -42,7 +42,7 @@ documentation_tab <- function() {
                      column(
                        6,
                        h3("Walkthrough video"),
-                       HTML('<iframe width="100%" height="300" src="//www.youtube.com/embed/5EWV9FlCyJ0?rel=0" frameborder="0"></iframe>'),
+                       HTML('<iframe width="100%" height="300" src="//www.youtube.com/embed/8JESp8J33XU" frameborder="0"></iframe>'),
                        p(class = "nb", "NB: This mini-app is for provided for demonstration purposes, is unsupported and is utilised at user's 
                        risk. If you plan to use this mini-app to inform your study, please review the code and ensure you are 
                        comfortable with the calculations made before proceeding. ")
