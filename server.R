2f3d69f22924ff377831587f17d8afb9d74043ad
annadom <dumey.23@gmail.com>
2020-12-18 09:55:44 +0000

    added more comments and fixed style

--- server.R
+++ server.R
@@ -27,23 +27,24 @@ function(input, output, session) {
    )
    
 
-   
+   # Filter options
 
-   filter_column <- callModule(choosenColumn, "column", data, label = "Choose column to filter by:")
-   filter_values <- callModule(columnValues, "column_value", data, column = filter_column)
-   filter_value <- callModule(chooseValue, "col_value", values = filter_values)
+   filter_column <- callModule(choosenColumn, "column", data, label = "Choose column to filter by:")    # Choose Column of the dataset
+   filter_values <- callModule(columnValues, "column_value", data, column = filter_column)              # Extract all the chosen column values
+   filter_value <- callModule(chooseValue, "col_value", values = filter_values)                         # Different UI according to chosen column type
 
    # String made to subset the subjects and display in the sidebar    
    filtering_string <- reactive({
-     if (is.numeric(filter_value())){
-        paste0(filter_column(), " ", input$condition, " ", filter_value()) 
-        
+      # If condition is numeric
+      if (is.numeric(filter_value())){
+         paste0(filter_column(), " ", input$condition, " ", filter_value()) 
+      # If condition uses a string  
       }else{
          paste0(filter_column(), " ", input$condition, "\ '", filter_value(), "\'")
       }
    })
    
-   # Text in the sidebar showing population subset 
+   # Text in the sidebar showing population subset
    output$condition <- renderText({
       filtering_string()
    })
@@ -51,8 +52,10 @@ function(input, output, session) {
    
    # Subsetting the dataset according to the condition
    subset_data <- reactive({
-      if (input$filtering == 0){ # If user does not want to subset, the complete dataset is used
+      # If no filters are applied - All the dataset is used
+      if (input$filtering == 0){
          return(data())
+      # When a filter is applied
       } else {
          filtered_data <- filter_(data(), filtering_string())
          return(filtered_data)
@@ -62,35 +65,39 @@ function(input, output, session) {
    
    # Tab 2 - TABLE OF CHARACTERISTICS ----------------------------------------------------------------------
    
-   stratification_table <- callModule(choosenColumn, "stratification", data, label = "Choose the stratification variable:")
-   variables_table <- callModule(choosenColumn, "variables", data, label = "Choose variables to add to the table:", multiple = TRUE)
+   stratification_table <- callModule(choosenColumn, "stratification", data, label = "Choose the stratification variable:")                # Display columns of the chosen dataset
+   variables_table <- callModule(choosenColumn, "variables", data, label = "Choose variables to add to the table:", multiple = TRUE)       # Display columns of the chosen dataset
    
    # Starting to elaborate the table of characteristics  
    tb <- reactive({
-      validate(need(variables_table(), "Please select characteristics to compare")) # Message if the user does not chose characteristics to compare
       
-      if (input$p == "Yes"){                                                      # If the user wants the p value this code will be active
+      # Message if the user does not chose characteristics to compare
+      validate(need(variables_table(), "Please select characteristics to compare"))
+      
+      # If the user wants the p value this code will be active
+      if (input$p == "Yes"){                                                      
          tableby(formulize(stratification_table(), x = variables_table()), data = subset_data())
+      # Hide p-value
       } else {
          my_controls <- tableby.control(test = FALSE)
          tableby(formulize(stratification_table(), x=variables_table()), data = subset_data(), control = my_controls)
       }        
    })
    
-   # Create the final table that will be displayed at the second tab
+   # Create the final table 
    output$tab <- renderTable({
       as.data.frame(summary(tb(), text = "html"))
-      
-   }, sanitize.text.function = identity
-   )
+   }, sanitize.text.function = identity)
    
    
    # Tab 3 - KEPLAN-MEIER ----------------------------------------------------------------------
-   endpoint <- callModule(choosenColumn, "endpoint", data, label = "Choose column that contains endpoint information:")
-   time <- callModule(choosenColumn, "time", data, label = "Choose column that contains survival time information:")
-   time_values <- callModule(columnValues, "time_values", data, column = time)
+   
+   
+   endpoint <- callModule(choosenColumn, "endpoint", data, label = "Choose column that contains endpoint information:")          # Display columns of the chosen dataset
+   time <- callModule(choosenColumn, "time", data, label = "Choose column that contains survival time information:")             # Display columns of the chosen dataset
+   time_values <- callModule(columnValues, "time_values", data, column = time)                                                   # Different UI according to chosen column type
 
-   stratification_kep <- callModule(choosenColumn, "stratification_kep", data, label = "Choose the stratification variable:")
+   stratification_kep <- callModule(choosenColumn, "stratification_kep", data, label = "Choose the stratification variable:")    # Display columns of the chosen dataset
    
 
    # Construct slider input 
@@ -107,6 +114,7 @@ function(input, output, session) {
    # Construct the Keplan-Meier plot 
    output$kep <- renderPlot({
       
+      # Need the stratification variable to build the Kaplan-Meier graph
       validate(need(stratification_kep(), "Please select stratification variable"))
       
       # Survival function - for ggsurvplot has to be inside the renderPlot function
@@ -127,13 +135,14 @@ function(input, output, session) {
    
    # Survival function outside renderPlot function
    runSur <- reactive({
+      # Need the stratification variable to build the kaplan meier graph, no message is passed to avoid having multiple messages on the screen
       validate(need(stratification_kep(), ""))
-      
       survfit(as.formula(paste('Surv(', time(), ',', endpoint(), ') ~ ', stratification_kep())), data=subset_data())
    })
    
    # Survival table
    output$survprob <- renderTable({
+      # Need the stratification variable to build the table, no message is passed to avoid having multiple messages on the screen
       validate(need(stratification_kep(), ""))
       
       table <- as.data.frame(summary(runSur(), times = input$xvalue)[c("surv", "time", "strata")]) 
@@ -143,8 +152,8 @@ function(input, output, session) {
    # Tab 4 - COX MODEL ----------------------------------------------------------------------
    
    
-   cox_variables <- callModule(choosenColumn, "cox_variables", data, label = "Choose variables to add to the model:", multiple = TRUE)
-   cox_strata <- callModule(choosenColumn, "cox_strata", data, label = "Choose strata to add to the model:", multiple = TRUE)
+   cox_variables <- callModule(choosenColumn, "cox_variables", data, label = "Choose variables to add to the model:", multiple = TRUE)    # Display columns of the chosen dataset
+   cox_strata <- callModule(choosenColumn, "cox_strata", data, label = "Choose strata to add to the model:", multiple = TRUE)             # Display columns of the chosen dataset
    
    
    cox_fit_text <- reactive({
